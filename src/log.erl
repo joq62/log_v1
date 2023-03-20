61d103a307df4bc4bf318c2af57da77e5e5fdbee
joq62 <jakim.leche@gmail.com>
2023-03-20 23:22:55 +0100

    corr

--- src/log.erl
+++ src/log.erl
@@ -13,7 +13,7 @@
 %% Internal exports
 %% --------------------------------------------------------------------
 -export([
-	 notice/5,warning/5,alert/5,debug/5,
+	 notice/6,warning/6,alert/6,debug/6,
 	 all_raw/0,all_raw/1,all_parsed/0,all_parsed/1,
 	 ping/0
 
@@ -41,29 +41,25 @@ all_parsed()->
 all_parsed(Severity)->
     rpc:call(node(),db_log,all_parsed,[Severity],5000).
 
-notice(Module,Function,Line,Key,Info)->
-    Node=node(),
+notice(Module,Function,Line,Node,Key,Info)->
     rpc:cast(node(),db_log,create,[os:system_time(?SystemTimeUnit),
 				   date(),time(),
 				   Module,Function,Line,Node,
 				   notice,Key,Info]).
 
-warning(Module,Function,Line,Key,Info)->
-    Node=node(),
+warning(Module,Function,Line,Node,Key,Info)->
     rpc:cast(node(),db_log,create,[os:system_time(?SystemTimeUnit),
 				   date(),time(),
 				   Module,Function,Line,Node,
 				   warning,Key,Info]).
 
-alert(Module,Function,Line,Key,Info)->
-    Node=node(),
+alert(Module,Function,Line,Node,Key,Info)->
     rpc:cast(node(),db_log,create,[os:system_time(?SystemTimeUnit),
 				   date(),time(),
 				   Module,Function,Line,Node,
 				   alert,Key,Info]).
 
-debug(Module,Function,Line,Key,Info)->
-    Node=node(),
+debug(Module,Function,Line,Node,Key,Info)->
     rpc:cast(node(),db_log,create,[os:system_time(?SystemTimeUnit),
 				   date(),time(),
 				   Module,Function,Line,Node,
--- tests/all.erl
+++ tests/all.erl
@@ -25,9 +25,9 @@ start([_ClusterSpec,_HostSpec])->
     io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
     
     ok=setup(),
-    ok=test_limit(),
+ %   ok=test_limit(),
  %   ok=test_local(),
-  %  ok=test_1(),
+    ok=test_1(),
    
     io:format("Stop OK !!! ~p~n",[{?MODULE,?FUNCTION_NAME}]),
     io:format(" init stop ~p~n",[init:stop()]),
@@ -126,6 +126,7 @@ test_1()->
   io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME,?LINE}]),
     AllNodes=test_nodes:get_nodes(),
     [N1,N2,N3,N4]=AllNodes,
+    Self=node(),
     %% Init
     io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME,?LINE}]),
       
@@ -136,12 +137,12 @@ test_1()->
 
     [N1]=lists:sort(rpc:call(N1,mnesia,system_info,[running_db_nodes],5000)),
      
-    sd:cast(log,log,notice,[?MODULE,?FUNCTION_NAME,?LINE,
+    sd:cast(log,log,notice,[?MODULE,?FUNCTION_NAME,?LINE,node(),
 			    "server start",
 			    [atom,"string",{term,1,2,"string"}]
 			   ]),
     [
-      {_,_,_,_,_,_,_,notice,"server start",[atom,"string",{term,1,2,"string"}]}
+      {_,_,_,_,_,_,Self,notice,"server start",[atom,"string",{term,1,2,"string"}]}
     ]=rpc:call(N1,log,all_raw,[],5000),
     io:format("N1 dist OK! ~p~n",[{?MODULE,?LINE}]),
 
@@ -154,19 +155,19 @@ test_1()->
     [N1,N2]=lists:sort(rpc:call(N2,mnesia,system_info,[running_db_nodes],5000)),
  %   yes=rpc:call(N2,mnesia,system_info,[],5000),
     [
-     {_,_,_,_,_,_,_,notice,"server start",[atom,"string",{term,1,2,"string"}]}
+     {_,_,_,_,_,_,Self,notice,"server start",[atom,"string",{term,1,2,"string"}]}
     ]=rpc:call(N2,log,all_raw,[],5000),
-    true=rpc:cast(N2,log,notice,[?MODULE,?FUNCTION_NAME,?LINE,
+    true=rpc:cast(N2,log,notice,[?MODULE,?FUNCTION_NAME,?LINE,node(),
 			    "event 1",[event_info1]
 			   ]),
     timer:sleep(1000),
     [
-     {_,_,_,_,_,_,_,notice,"event 1",[event_info1]},
-     {_,_,_,_,_,_,_,notice,"server start",[atom,"string",{term,1,2,"string"}]}
+     {_,_,_,_,_,_,Self,notice,"server start",[atom,"string",{term,1,2,"string"}]},
+     {_,_,_,_,_,_,Self,notice,"event 1",[event_info1]}    
     ]=rpc:call(N1,log,all_raw,[],5000),
     [
-     {_,_,_,_,_,_,_,notice,"event 1",[event_info1]},
-     {_,_,_,_,_,_,_,notice,"server start",[atom,"string",{term,1,2,"string"}]}
+     {_,_,_,_,_,_,Self,notice,"server start",[atom,"string",{term,1,2,"string"}]},
+     {_,_,_,_,_,_,Self,notice,"event 1",[event_info1]}    
     ]=rpc:call(N2,log,all_raw,[],5000),
     
     io:format("N2 dist OK! ~p~n",[{?MODULE,?LINE}]),
@@ -181,9 +182,9 @@ test_1()->
     [N1,N2,N3]=lists:sort(rpc:call(N2,mnesia,system_info,[running_db_nodes],5000)),
     [N1,N2,N3]=lists:sort(rpc:call(N3,mnesia,system_info,[running_db_nodes],5000)),
 
-    true=rpc:cast(N1,log,notice,[?MODULE,?FUNCTION_NAME,?LINE,"event 2",[event_info2]]),
-    true=rpc:cast(N2,log,warning,[?MODULE,?FUNCTION_NAME,?LINE,"warning1",[warning_info1]]),
-    true=rpc:cast(N3,log,alert,[?MODULE,?FUNCTION_NAME,?LINE,"alert 1",[alert_info1]]),
+    true=rpc:cast(N1,log,notice,[?MODULE,?FUNCTION_NAME,?LINE,node(),"event 2",[event_info2]]),
+    true=rpc:cast(N2,log,warning,[?MODULE,?FUNCTION_NAME,?LINE,node(),"warning1",[warning_info1]]),
+    true=rpc:cast(N3,log,alert,[?MODULE,?FUNCTION_NAME,?LINE,node(),"alert 1",[alert_info1]]),
 
  %   yes=rpc:call(N3,mnesia,system_info,[],5000),
     io:format("N3 dist OK! ~p~n",[{?MODULE,?LINE}]),
@@ -196,12 +197,13 @@ test_1()->
     [N1,N2,N3,N4]=lists:sort(rpc:call(N3,mnesia,system_info,[running_db_nodes],5000)),
     [N1,N2,N3,N4]=lists:sort(rpc:call(N4,mnesia,system_info,[running_db_nodes],5000)),
    
-    true=rpc:cast(N4,log,debug,[?MODULE,?FUNCTION_NAME,?LINE,"debug 1",[debug_info1]]),
+    true=rpc:cast(N4,log,debug,[?MODULE,?FUNCTION_NAME,?LINE,node(),"debug 1",[debug_info1]]),
 
     [
-     {_,_,_,_,_,_,_,notice,"event 2",[event_info2]},
+     {_,_,_,_,_,_,_,notice,"server start",[atom,"string",{term,1,2,"string"}]},
      {_,_,_,_,_,_,_,notice,"event 1",[event_info1]},
-     {_,_,_,_,_,_,_,notice,"server start",[atom,"string",{term,1,2,"string"}]}
+     {_,_,_,_,_,_,_,notice,"event 2",[event_info2]}    
+    
     ]=rpc:call(N4,log,all_raw,[notice],5000),
     [
      {_,_,_,_,_,_,_,warning,"warning1",[warning_info1]}
